d4fe4a2a01189e7de2ae492bc5b4b67685475c29
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-10 19:05:32 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+  def initialize(dog_name, dog_breed)
+    @dog_name = dog_name
+    @dog_breed = dog_breed
+  end
+  def name=(dogs_name)
+    @dog_name
+  end
+  def name
+    "#{@dog_name}"
+end
